24c9d44c878f7d2784d2dac3ec6a4b7b1f3c4254
Emily <1084355983@qq.com>
2016-04-16 10:51:47 +0800

    客户端

---
+++ Socket通信_Client/Socket通信_Client/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Socket通信_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(13, 13);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(100, 21);
+            this.txtServer.TabIndex = 0;
+            this.txtServer.Text = "192.168.0.102";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(130, 13);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(62, 21);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "50000";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(286, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "连接";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 41);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(348, 68);
+            this.txtLog.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(13, 116);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(348, 130);
+            this.txtMsg.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 287);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMsg;
+    }
+}
+
---
+++ Socket通信_Client/Socket通信_Client/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socket通信_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Socket socketSend;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //创建负责通信的Socket
+            socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(txtServer.Text);
+            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+            //获得要连接的远程服务器应用程序的IP地址和端口号
+            socketSend.Connect(point);
+            ShowMsg("连接成功");
+
+            Thread th = new Thread(Recive);
+            th.IsBackground = true;
+            th.Start();
+        }
+        void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = txtMsg.Text.Trim();
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+            socketSend.Send(buffer);
+        }
+        /// <summary>
+        /// 不停的接收服务器发来的消息
+        /// </summary>
+        void Recive()
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024 * 1024 * 2];
+                int r = socketSend.Receive(buffer);
+                //实际接收到的有效字节数
+                if (r == 0)
+                {
+                    break;
+                }
+
+                if (buffer[0] == 0)
+                {
+
+                    string s = Encoding.UTF8.GetString(buffer, 1, r - 1);
+                    ShowMsg(socketSend.RemoteEndPoint + ":" + s);
+                }
+                else if (buffer[0] == 1)
+                {
+                    SaveFileDialog sfd = new SaveFileDialog();
+                    sfd.InitialDirectory = @"C:\Users\Xuheyao\Desktop";
+                    sfd.Title = "请选择要保存的文件";
+                    sfd.Filter = "所有文件|*.*";
+                    sfd.ShowDialog(this);
+
+                    string path = sfd.FileName;
+                    using (FileStream fsWrite = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        fsWrite.Write(buffer, 1, r - 1);
+                    }
+                    MessageBox.Show("保存成功");
+                }
+                else if (buffer[0] == 2)
+                {
+                    ZD();
+                }
+            }
+        }
+        /// <summary>
+        /// 震动
+        /// </summary>
+        void ZD()
+        {
+            Point p = this.Location;
+            int x = p.X;
+            int y = p.Y;
+            int j;
+            for (int i = 0; i < 200; i++)
+            {
+                this.Location = new Point(x, y + 5);
+                for (j = 0; j < 10000; j++) ;
+                this.Location = new Point(x + 5, y + 5);
+                for (j = 0; j < 10000; j++) ;
+                this.Location = new Point(x + 5, y);
+                for (j = 0; j < 10000; j++) ;
+                this.Location = new Point(x, y + 5);
+                for (j = 0; j < 10000; j++) ;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
