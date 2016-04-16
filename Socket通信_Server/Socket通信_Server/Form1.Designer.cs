ca3ed373b3396af9d9227810419b5fb98ef25db6
Emily <1084355983@qq.com>
2016-04-16 10:56:27 +0800

    服务器端

---
+++ Socket通信_Server/Socket通信_Server/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Socket通信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ip = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ip
+            // 
+            this.ip.Location = new System.Drawing.Point(13, 13);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(100, 21);
+            this.ip.TabIndex = 0;
+            this.ip.Text = "192.168.0.102";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(120, 13);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(57, 21);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "50000";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(11, 40);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(389, 81);
+            this.txtLog.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(325, 11);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始监听";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 182);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(389, 104);
+            this.txtMsg.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(11, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 20);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(246, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "打开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(13, 127);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(227, 21);
+            this.txtPath.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "发送文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(245, 292);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "震动";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 322);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.ip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ip;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Socket通信_Server/Socket通信_Server/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socket通信
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //点击开始监听的时候 在服务器端创建
+                //一个负责监听IP地址跟端口号的Socket
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Any;
+                //创建端口号对象
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+                //监听
+                socketWatch.Bind(point);
+                ShowMsg("监听成功");
+                socketWatch.Listen(10);
+
+                Thread th = new Thread(Listen);
+                th.IsBackground = true;
+                th.Start(socketWatch);
+            }
+            catch
+            {
+
+            }
+        }
+        Socket socketSend;
+
+        //将远程连接的客户端的IP地址和Socket存入集合中
+        Dictionary<string, Socket> dicSocket = new Dictionary<string, Socket>();
+        /// <summary>
+        /// 等待客户端的连接 并且创建与之通信用的Socket
+        /// </summary>
+        void Listen(Object o)
+        {
+            Socket socketWatch = o as Socket;
+            while (true)
+            {
+                try
+                {
+                    //等待客户端连接，并创建一个负责通信的Socket
+                    socketSend = socketWatch.Accept();
+                    
+                    /////////////////////
+                    dicSocket.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
+                    comboBox1.Items.Add(socketSend.RemoteEndPoint.ToString());
+                    //192.168...连接成功
+                    ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+                    //开启一个新线程，不停的接收客户端发送过来的消息
+                    Thread th = new Thread(Recive);
+                    th.IsBackground = true;
+                    th.Start(socketSend);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        
+        /// <summary>
+        /// 服务端不停的接收客户端发送过来的消息
+        /// </summary>
+        /// <param name="o"></param>
+        void Recive(object o)
+        {
+            Socket socketSend = o as Socket;
+            while (true)
+            {
+                try
+                {
+                    //客户端连接成功后，服务器应该接受客户端发来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 2];
+                    //实际接收到的有效字节数
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                        break;
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);
+                    ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        /// <summary>
+        /// 服务器给客户端发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = txtMsg.Text;
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+                List<byte> list = new List<byte>();
+                list.Add(0);
+                list.AddRange(buffer);
+                byte[] newBuffer = list.ToArray();
+                //获得用户在下拉框中选中的IP地址
+                string ip = comboBox1.SelectedItem.ToString();
+                dicSocket[ip].Send(newBuffer);
+                //socketSend.Send(buffer);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = @"C:\Users\Xuheyao\Desktop";
+            ofd.Title = "请选择文件";
+            ofd.Filter = "所有文件|*.*";
+            ofd.ShowDialog();
+
+            txtPath.Text = ofd.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path=txtPath.Text;
+            using(FileStream fsRead=new FileStream(path,FileMode.OpenOrCreate,FileAccess.Read))
+            {
+                byte[] buffer = new byte[1024 * 1024 * 5];
+                List<byte> list = new List<byte>();
+                list.Add(1);
+                list.AddRange(buffer);
+                byte[] newBuffer = list.ToArray();
+                int r = fsRead.Read(buffer, 0, buffer.Length);
+                dicSocket[comboBox1.SelectedItem.ToString()].Send(buffer, 0, r + 1,SocketFlags.None);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = new byte[1];
+            buffer[0] = 2;
+            dicSocket[comboBox1.SelectedItem.ToString()].Send(buffer);
+        }
+    }
+}
